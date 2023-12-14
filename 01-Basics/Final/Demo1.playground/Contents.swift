5d1454bd73f5f54299e1594e0f49640f411d4bb9
Walter Tyree <walter@tyreeapps.com>
2023-12-14 18:11:45 -0500

    TE edits

--- 01-Basics/Final/Demo1.playground/Contents.swift
+++ 01-Basics/Final/Demo1.playground/Contents.swift
@@ -3,23 +3,23 @@ let int2: Int = 10
 
 // int1 = 6
 
-var intSum = int1 + int2
-var intSubtract = int2 - int1
-var intMultiply = int1 * int2
-var intDivide = int2 / int1
+let intSum = int1 + int2
+let intSubtract = int2 - int1
+let intMultiply = int1 * int2
+let intDivide = int2 / int1
 
-let float1: Float = 6
-let float2: Float = 11
+let float1: Float = 6.0
+let float2: Float = 11.0
 
-var floatSum = float1 + float2
-var floatSubtract = float2 - float1
-var floatMultiply = float1 * float2
-var floatDivide = float2 / float1
+let floatSum = float1 + float2
+let floatSubtract = float2 - float1
+let floatMultiply = float1 * float2
+let floatDivide = float2 / float1
 
 let double1 = 6.0
 let double2: Double = 11.0
 
-var doubleSum = double1 + double2
-var doubleSubtract = double2 - double1
-var doubleMultiply = double1 * double2
-var doubleDivide = double2 / double1
+let doubleSum = double1 + double2
+let doubleSubtract = double2 - double1
+let doubleMultiply = double1 * double2
+let doubleDivide = double2 / double1
--- 01-Basics/Final/Demo2.playground/Contents.swift
+++ 01-Basics/Final/Demo2.playground/Contents.swift
@@ -1,8 +1,8 @@
 let welcomeMessage_part1 = "Hello "
-var username = "Raymond"
+let username = "Raymond"
 let welcomeMessage_part2 = ", good to see you today."
 
-var message = welcomeMessage_part1 + username + welcomeMessage_part2
+let message = welcomeMessage_part1 + username + welcomeMessage_part2
 
 print(message)
 print(message.count)
@@ -17,23 +17,26 @@ print("Hello \(username), good to see you today.")
 let int1 = 5
 let int2: Int = 10
 
-var intSum = int1 + int2
+let intSum = int1 + int2
 print("Integer sum of \(int1) and \(int2) is: \(intSum)")
-var intSubtract = int2 - int1
+
+let intSubtract = int2 - int1
 print("Integer subtraction of \(int1) from \(int2) is: \(intSubtract)")
-var intMultiply = int1 * int2
+
+let intMultiply = int1 * int2
 print("Integer multiplication of \(int1) and \(int2) is: \(intMultiply)")
-var intDivide = int2 / int1
+
+let intDivide = int2 / int1
 print("Integer division of \(int1) and \(int2) is: \(intDivide)")
 
 let float1: Float = 6
 let float2: Float = 11
 
-var floatDivide = float2 / float1
-print("Floating Point division of \(float1) and \(float2) is: \(floatDivide)")
+let floatDivide = float2 / float1
+print("Floating-point division of \(float1) and \(float2) is: \(floatDivide)")
 
 let double1 = 6.0
 let double2: Double = 11.0
 
-var doubleDivide = double2 / double1
-print("Double floating Point division of \(double1) and \(double2) is: \(doubleDivide)")
+let doubleDivide = double2 / double1
+print("Double-precision floating-point division of \(double1) and \(double2) is: \(doubleDivide)")
