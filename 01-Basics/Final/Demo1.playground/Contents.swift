520e4efd4fb26006af3ca6c1f8c69a3fc77ae57d
Ehab Amer <ehab.amer@gmail.com>
2023-12-18 13:16:32 +0100

    Update Contents.swift

--- 01-Basics/Final/Demo1.playground/Contents.swift
+++ 01-Basics/Final/Demo1.playground/Contents.swift
@@ -8,17 +8,17 @@ let boolNumbersAreEqual: Bool = int1 == int2
 let boolNumbersAreDescending = int1 > int2
 
 let intSum = int1 + int2
-let intSubtract = int2 - int1
+let intSubtract = int1 - int2
 let intMultiply = int1 * int2
-let intDivide = int2 / int1
+let intDivide = int1 / int2
 
 let float1: Float = 11.0
 let float2: Float = 6.0
 
 let floatSum = float1 + float2
-let floatSubtract = float2 - float1
+let floatSubtract = float1 - float2
 let floatMultiply = float1 * float2
-let floatDivide = float2 / float1
+let floatDivide = float1 / float2
 
 let double1: Double = 11.0
 let double2 = 6.0
